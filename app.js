983df325deeb60cd92279c6d157394b9a916598e
Song Ji <songji0509@gmail.com>
2016-11-06 13:51:32 -0800

    Basic file system functions testing.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,6 +17,52 @@ var helpers = require('express-helpers')(app);
 
 /* Set up Socket IO*/
 app.io = require('socket.io')();
+
+/* File system */
+
+// // this will read and log file content ayncally
+// fs.readFile('input.txt', function(err, data){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log('Async data is' + data.toString());
+//   }
+// });
+// 
+// // Syc read files, this will excute at once
+// var data = fs.readFileSync('input.txt');
+// console.log('Sync data is' + data.toString());
+// 
+// 
+// // This will create a tradbale stream by chuncks from input.txt
+// var readableStream = fs.createReadStream('input.txt');
+// var data = '';
+// readableStream.setEncoding('UTF8');
+// readableStream.on('data', function(chunk){
+//   data += chunk;
+// });
+// 
+// readableStream.on('end', function(){
+//   console.log(data);
+// })
+
+// // This will create a new file called output.txt and insert write data to it
+// var writeData = 'Hello World';
+// var writeableStram = fs.createWriteStream('output.txt');
+// writeableStram.write(writeData,'UTF8');
+// 
+// writeableStram.end();
+// writeableStram.on('finish',function(){
+//   console.log('Write Completed');
+// })
+
+// Pipes
+// this code will bring all content from input.txt to output.txt
+var readableStream = fs.createReadStream('input.txt');
+var writeableStram = fs.createWriteStream('output.txt');
+readableStream.pipe(writeableStram);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
