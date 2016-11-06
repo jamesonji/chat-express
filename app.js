07640c3596171a1374ea4485c1d2ec155270059c
Song Ji <songji0509@gmail.com>
2016-11-05 18:33:45 -0700

    Embed ace editor in index page

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/ace-builds', express.static(__dirname + '/node_modules/ace-builds'));
 
 app.use('/', index);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
