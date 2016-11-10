624cb6bdca167820d03b465aaf484abc7022c1d6
Song Ji <songji0509@gmail.com>
2016-11-09 17:30:06 -0800

    Change layout, add jQuery add editor code

--- public/javascripts/ace.js
+++ public/javascripts/ace.js
@@ -1,4 +1,18 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
-editor.setShowPrintMargin(false);
+// pass in the editor id number
+var addEditor = function( num ){
+  var editorId = 'editor-' + num;
+  var editor = ace.edit(editorId);
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
+  editor.setShowPrintMargin(false);
+}
+
+
+
+$('#add-editor').click(function(){
+  console.log('Add editor is working!');
+  var editorCount = $('.ace-editor').children().length;
+  var nextId = editorCount + 1;
+  $('.ace-editor').append("<div id= editor-'"+ nextId + "'></div>");
+  addEditor(nextId);
+});
