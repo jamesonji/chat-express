9d3511ecf6f1f7131d19a2a670ef05ce1b31f6d3
Song Ji <songji0509@gmail.com>
2016-11-09 21:30:07 -0800

    Add a couple settings and refactored the code

--- public/javascripts/ace.js
+++ public/javascripts/ace.js
@@ -1,16 +1,28 @@
+var EDITORCOUNT = 0;
+
 // pass in the editor id number
 var addEditor = function( num ){
+  // create a editor id in the form of 'editor-n'
   var editorId = 'editor-' + num;
+  // create a editor with the id provided above
   var editor = ace.edit(editorId);
+  // set the editor theme
   editor.setTheme("ace/theme/monokai");
+  // set the editor mode
   editor.getSession().setMode("ace/mode/javascript");
+  
+  editor.getSession().setUseWrapMode(true);
+
+  editor.setHighlightActiveLine(true);
+
+  // don't show the margin
   editor.setShowPrintMargin(false);
 }
 
 $('#add-editor').click(function(){
-  console.log('Add editor is working!');
-  var editorCount = $('.ace-editor').children().length;
-  var nextId = editorCount + 1;
-  $('.editor-container').append("<div class='ace-editor' id= 'editor-"+ nextId + "'></div>");
-  addEditor(nextId);
+  // check how many ace-editor exists in the form
+  // var editorCount = $('.ace-editor').children().length;
+  EDITORCOUNT += 1;
+  $('.editor-container').append("<div class='ace-editor' id= 'editor-"+ EDITORCOUNT + "'></div>");
+  addEditor(EDITORCOUNT);
 });
