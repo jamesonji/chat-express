8e26daaa84405d5cb986abd1853e6aa6f952ef48
Song Ji <songji0509@gmail.com>
2016-11-10 17:27:21 -0800

    Add MongoDB testing code

--- app.js
+++ app.js
@@ -18,6 +18,39 @@ var app = express();
 // Include Ejs helpers
 var helpers = require('express-helpers')(app);
 
+//---------  Setup Mongo DB ----------------
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
+//  
+// // Connection URL 
+// var url = 'mongodb://localhost:27017/chat-express';
+// // Use connect method to connect to the Server 
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
+//  
+//   insertDocuments(db, function() {
+//     db.close();
+//   });
+// }); 
+// 
+// var insertDocuments = function(db, callback) {
+//   // Get the documents collection 
+//   var collection = db.collection('documents');
+//   // Insert some documents 
+//   collection.insertMany([
+//     {a : 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the document collection");
+//     callback(result);
+//   });
+// }
+
+//--- End of Mongo DB -------------------
+
 /* Setup Redis*/ 
 
 
@@ -85,23 +118,23 @@ app.io.on('connection', function (socket) {
 /*
  * Redis Pub/Sub connection
 */
-subscriber.on("subscribe", function (channel, count) {
-    publisher.publish("a nice channel", "I am sending a message.");
-    publisher.publish("a nice channel", "I am sending a second message.");
-    publisher.publish("a nice channel", "I am sending my last message.");
-});
-
-subscriber.on("message", function (channel, message) {
-    console.log("sub channel " + channel + ": " + message);
-    msg_count += 1;
-    if (msg_count === 3) {
-        subscriber.unsubscribe();
-        subscriber.quit();
-        publisher.quit();
-    }
-});
-
-subscriber.subscribe("a nice channel");
+// subscriber.on("subscribe", function (channel, count) {
+//     publisher.publish("a nice channel", "I am sending a message.");
+//     publisher.publish("a nice channel", "I am sending a second message.");
+//     publisher.publish("a nice channel", "I am sending my last message.");
+// });
+// 
+// subscriber.on("message", function (channel, message) {
+//     console.log("sub channel " + channel + ": " + message);
+//     msg_count += 1;
+//     if (msg_count === 3) {
+//         subscriber.unsubscribe();
+//         subscriber.quit();
+//         publisher.quit();
+//     }
+// });
+// 
+// subscriber.subscribe("a nice channel");
 
 
 module.exports = app;
